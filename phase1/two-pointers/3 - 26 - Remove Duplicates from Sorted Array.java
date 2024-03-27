5ec21b14e7a1342a5cf04b1404958d85a11988b1
ronilchristian <ronilrc13@gmail.com>
2024-03-27 12:38:31 -0500

    add phase 1 problem 26 - Remove Duplicates from Sorted Array

---
+++ phase1/two-pointers/3 - 26 - Remove Duplicates from Sorted Array.java
@@ -0,0 +1,22 @@
+// 3 - 26 - Remove Duplicates from Sorted Array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // base case
+        int n = nums.length;
+        if (n < 2) return n;
+
+        // define pointers
+        int start = 0, end = 1;
+
+        // iterate
+        while (end < n) {
+            if (nums[start] != nums[end]) {
+                nums[++start] = nums[end];
+            }
+            end++;
+        }
+
+        return start + 1;
+    }
+}
